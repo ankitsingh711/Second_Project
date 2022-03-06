00f42eccbe73ed752ef096ac2756f0b7638226b2
ankit711 <itsankitsingh711@edureka.co>
2022-03-06 18:02:56 +0530

    some changes

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-function changemode(){
-    var mybody = document.body;
-    mybody.classList.toggle('darkmode')
-}
-
 function loadcoupon(){
     document.getElementById("coupon").style.visibility="visible"
 }
 
 function closecoupon(){
     document.getElementById("coupon").style.visibility="hidden"
+}
+
+function changeMode(){
+    
 }
