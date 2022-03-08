845366317a9f7c35cc035fabed116050e078177e
ankit711 <itsankitsingh711@edureka.co>
2022-03-08 13:00:28 +0530

    javascript

--- index.js
+++ index.js
@@ -11,26 +11,17 @@ function changeMode(){
     document.body.classList.toggle("darkmode")
 }
 
-var x = document.getElementById("output")
-var y = document.getElementById("weatheroutput")
+var x = document.getElementById("out").innerHTML=`Latitude ${data.coords.latitude} Longitude ${data.coords.longitude}`
+var y = document.getElementById("weatherout").innerHTML= output;
+
 function geolocation(){
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition)
-    }
+    }else{x.innerHTML="GeoLocation is not supported by this Browser.";}
 }
-
-function loadlocation(){
-    document.getElementById("location").visibility="visible"
-}
-
-function closelocation(){
-    document.getElementById("location").visibility="visible"
-}
-
 
 function showPosition(data){
     console.log(data)
-    x.innerText = `Latitude is ${data.coords.latitude}, Longitude is ${data.coords.longitude}`
     let latitude = data.coords.latitude;
     let longitude = data.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
@@ -41,7 +32,16 @@ function showPosition(data){
         let weather = `${data.list[0].temp.day} degree`
         let output = `${cityName} ${weather}`
         console.log(data)
-        y.innerText= output;
     })
 }
+
+function loadlocation(){
+    document.getElementById("location").style.visibility="visible"
+}
+
+function closelocation(){
+    document.getElementById("location").style.visibility="visible"
+}
+
+window.onload= loadlocation()
 
